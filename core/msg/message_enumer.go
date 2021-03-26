6fd02a71774f696969d905fe224b37bf2daf26fb
andig <cpuidle@gmx.de>
2021-03-26 09:58:22 +0100

    wip

--- core/msg/message_enumer.go
+++ core/msg/message_enumer.go
@@ -7,9 +7,9 @@ import (
 	"fmt"
 )
 
-const _MessageName = "ErrorWarnBatteryConfiguredBatteryPowerBatterySoCGridConfiguredGridCurrentsGridPowerPrioritySoCPvConfiguredPvPowerSiteTitleActivePhasesChargeConfiguredChargeCurrentChargeCurrentsChargedEnergyChargeDurationChargeEstimateChargePowerChargeRemainingEnergyChargingClimaterConnectedConnectedDurationEnabledHasVehicleMaxCurrentMinCurrentMinSoCModePhasesRangeRemoteDisabledRemoteDisabledSourceSocCapacitySocChargeSocLevelsSocTitleTargetSoCTargetTimeTimerActiveTimerSetTitle"
+const _MessageName = "ErrorWarnAvailableVersionReleaseNotesBatteryConfiguredBatteryPowerBatterySoCGridConfiguredGridCurrentsGridPowerPrioritySoCPvConfiguredPvPowerSiteTitleActivePhasesChargeConfiguredChargeCurrentChargeCurrentsChargedEnergyChargeDurationChargeEstimateChargePowerChargeRemainingEnergyChargingClimaterConnectedConnectedDurationEnabledHasVehicleMaxCurrentMinCurrentMinSoCModePhasesRangeRemoteDisabledRemoteDisabledSourceSocCapacitySocChargeSocLevelsSocTitleTargetSoCTargetTimeTimerActiveTimerSetTitle"
 
-var _MessageIndex = [...]uint16{0, 5, 9, 26, 38, 48, 62, 74, 83, 94, 106, 113, 122, 134, 150, 163, 177, 190, 204, 218, 229, 250, 258, 266, 275, 292, 299, 309, 319, 329, 335, 339, 345, 350, 364, 384, 395, 404, 413, 421, 430, 440, 451, 459, 464}
+var _MessageIndex = [...]uint16{0, 5, 9, 25, 37, 54, 66, 76, 90, 102, 111, 122, 134, 141, 150, 162, 178, 191, 205, 218, 232, 246, 257, 278, 286, 294, 303, 320, 327, 337, 347, 357, 363, 367, 373, 378, 392, 412, 423, 432, 441, 449, 458, 468, 479, 487, 492}
 
 func (i Message) String() string {
 	i -= 1
@@ -19,53 +19,55 @@ func (i Message) String() string {
 	return _MessageName[_MessageIndex[i]:_MessageIndex[i+1]]
 }
 
-var _MessageValues = []Message{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44}
+var _MessageValues = []Message{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46}
 
 var _MessageNameToValueMap = map[string]Message{
 	_MessageName[0:5]:     1,
 	_MessageName[5:9]:     2,
-	_MessageName[9:26]:    3,
-	_MessageName[26:38]:   4,
-	_MessageName[38:48]:   5,
-	_MessageName[48:62]:   6,
-	_MessageName[62:74]:   7,
-	_MessageName[74:83]:   8,
-	_MessageName[83:94]:   9,
-	_MessageName[94:106]:  10,
-	_MessageName[106:113]: 11,
-	_MessageName[113:122]: 12,
-	_MessageName[122:134]: 13,
-	_MessageName[134:150]: 14,
-	_MessageName[150:163]: 15,
-	_MessageName[163:177]: 16,
-	_MessageName[177:190]: 17,
-	_MessageName[190:204]: 18,
-	_MessageName[204:218]: 19,
-	_MessageName[218:229]: 20,
-	_MessageName[229:250]: 21,
-	_MessageName[250:258]: 22,
-	_MessageName[258:266]: 23,
-	_MessageName[266:275]: 24,
-	_MessageName[275:292]: 25,
-	_MessageName[292:299]: 26,
-	_MessageName[299:309]: 27,
-	_MessageName[309:319]: 28,
-	_MessageName[319:329]: 29,
-	_MessageName[329:335]: 30,
-	_MessageName[335:339]: 31,
-	_MessageName[339:345]: 32,
-	_MessageName[345:350]: 33,
-	_MessageName[350:364]: 34,
-	_MessageName[364:384]: 35,
-	_MessageName[384:395]: 36,
-	_MessageName[395:404]: 37,
-	_MessageName[404:413]: 38,
-	_MessageName[413:421]: 39,
-	_MessageName[421:430]: 40,
-	_MessageName[430:440]: 41,
-	_MessageName[440:451]: 42,
-	_MessageName[451:459]: 43,
-	_MessageName[459:464]: 44,
+	_MessageName[9:25]:    3,
+	_MessageName[25:37]:   4,
+	_MessageName[37:54]:   5,
+	_MessageName[54:66]:   6,
+	_MessageName[66:76]:   7,
+	_MessageName[76:90]:   8,
+	_MessageName[90:102]:  9,
+	_MessageName[102:111]: 10,
+	_MessageName[111:122]: 11,
+	_MessageName[122:134]: 12,
+	_MessageName[134:141]: 13,
+	_MessageName[141:150]: 14,
+	_MessageName[150:162]: 15,
+	_MessageName[162:178]: 16,
+	_MessageName[178:191]: 17,
+	_MessageName[191:205]: 18,
+	_MessageName[205:218]: 19,
+	_MessageName[218:232]: 20,
+	_MessageName[232:246]: 21,
+	_MessageName[246:257]: 22,
+	_MessageName[257:278]: 23,
+	_MessageName[278:286]: 24,
+	_MessageName[286:294]: 25,
+	_MessageName[294:303]: 26,
+	_MessageName[303:320]: 27,
+	_MessageName[320:327]: 28,
+	_MessageName[327:337]: 29,
+	_MessageName[337:347]: 30,
+	_MessageName[347:357]: 31,
+	_MessageName[357:363]: 32,
+	_MessageName[363:367]: 33,
+	_MessageName[367:373]: 34,
+	_MessageName[373:378]: 35,
+	_MessageName[378:392]: 36,
+	_MessageName[392:412]: 37,
+	_MessageName[412:423]: 38,
+	_MessageName[423:432]: 39,
+	_MessageName[432:441]: 40,
+	_MessageName[441:449]: 41,
+	_MessageName[449:458]: 42,
+	_MessageName[458:468]: 43,
+	_MessageName[468:479]: 44,
+	_MessageName[479:487]: 45,
+	_MessageName[487:492]: 46,
 }
 
 // MessageString retrieves an enum value from the enum constants string name.
